a8922f303a69726027a5ab727d68788b7f21049d
Ruhul Amin Sardar <ruhul.sardar143@gmail.com>
2022-03-28 17:10:17 +0530

    Initial seeds added and showing in a page and the route created

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelpCamp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
 })
 
 app.listen(3000, () => {
